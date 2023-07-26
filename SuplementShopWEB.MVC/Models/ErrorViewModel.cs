3c84431cd4de68ebe3ccef88dad6bd063c5261ab
7LinesOfCode <jjjiw7ct@gmail.com>
2023-07-26 13:06:07 +0200

    Add project files.

---
+++ SuplementShopWEB.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SuplementShopWEB.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
