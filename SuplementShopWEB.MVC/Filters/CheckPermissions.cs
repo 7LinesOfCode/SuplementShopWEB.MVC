5dfda26a59705fecdbf79f73d45504b2b8b1dedc
7LinesOfCode <jjjiw7ct@gmail.com>
2023-09-02 15:51:30 +0200

    Added atribute for checkpermsion method/class - In Progres

---
+++ SuplementShopWEB.MVC/Filters/CheckPermissions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace SuplementShopWEB.MVC.Filters
+{
+    public class CheckPermissions : Attribute, IAuthorizationFilter
+    { // In progress
+        private readonly string _permission;
+        public CheckPermissions(string permission)
+        {
+            _permission= permission;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            bool isAuthorized = CheckUserPermision(context.HttpContext.User, _permission);
+        }
+
+        private bool CheckUserPermision(ClaimsPrincipal user, string permission)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
